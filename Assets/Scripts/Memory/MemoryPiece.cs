9f2c540a4712b5da569cdc9d33912de7f9ee6855
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2024-11-21 13:59:03 +0100

    Clean up project and add functionality from 20.11.2024

--- Assets/Scripts/Memory/MemoryPiece.cs
+++ Assets/Scripts/Memory/MemoryPiece.cs
@@ -7,13 +7,23 @@ using UnityEngine.EventSystems;
 public class MemoryPiece : MonoBehaviour, IPointerClickHandler
 {
     public MemoryManager manager;
+    public Sprite memoryImage;
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Piece clicked: " + eventData.pointerClick.gameObject.name);
-        manager.selected = this;
-        //manager.cardsOpen++;
-        //manager.cardsOpen += 1;
+        if (manager.cardsOpen == 0)
+        {
+            // card1
+            manager.card1 = GetComponent<MemoryPiece>();
+        }
+        if (manager.cardsOpen == 1)
+        {
+            // card2
+            manager.card2 = this;
+        }
         manager.cardsOpen = manager.cardsOpen + 1;
+
+        GetComponent<Image>().sprite = memoryImage;
     }
 }
--- Assets/Scripts/MemoryManager.cs
+++ Assets/Scripts/MemoryManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class MemoryManager : MonoBehaviour
 {
+    public int score;
+
     // 1. Ein Prefab angeben das dupliziert werden soll
     public GameObject memoryPiece;
     // 2. Das Transform unter das meine memorypieces geparented werden sollen
@@ -14,7 +16,9 @@ public class MemoryManager : MonoBehaviour
 
     public MemoryManager mm;
 
-    public MemoryPiece selected;
+    public MemoryPiece card1;
+    public MemoryPiece card2;
+
     public int cardsOpen = 0;
     public int playerNumber = 1;
 
@@ -27,18 +31,12 @@ public class MemoryManager : MonoBehaviour
             GameObject piece = Instantiate(memoryPiece);
             piece.name = "Memory" + i + "_1";
             piece.GetComponent<MemoryPiece>().manager = mm;
+            piece.GetComponent<MemoryPiece>().memoryImage = images[i];
+
             GameObject piece2 = Instantiate(memoryPiece);
             piece2.name = "Memory" + i + "_2";
             piece2.GetComponent<MemoryPiece>().manager = mm;
-
-            // 5. Von dem neu erstellten GameObject möchten
-            // wir den Image Component in einer Variable abspeichern
-            Image img = piece.GetComponent<Image>();
-            Image img2 = piece2.GetComponent<Image>();
-
-            // 6. Wir möchten in den Image Component das sprite aus dem array laden.
-            img.sprite = images[i];
-            img2.sprite = images[i];
+            piece2.GetComponent<MemoryPiece>().memoryImage = images[i];
 
             // 7. Das neu erstellte GameObject muss unter das
             // parent transform gesetzt werden (SetParent  von einem Transform Object)
@@ -49,6 +47,33 @@ public class MemoryManager : MonoBehaviour
 
     private void Update()
     {
+        if (cardsOpen == 2)
+        {
+            Sprite bild1 = card1.GetComponent<Image>().sprite;
+            Sprite bild2 = card2.GetComponent<Image>().sprite;
+
+            if (bild1 == bild2)
+            {
+                Debug.Log("Richtig");
+                score++;
+                Destroy(card1.gameObject);
+                Destroy(card2.gameObject);
+
+                if (score == images.Length)
+                {
+                    Debug.Log("Wir haben gewonnen!");
+                }
+            }
+            else
+            {
+                Debug.Log("Falsch");
+            }
+
+            // card1.gameObject
+
+            cardsOpen = 0;
+        }
+
         if (Input.GetKeyDown("space"))
         {
             Transform[] children = content.GetComponentsInChildren<Transform>();
@@ -57,6 +82,21 @@ public class MemoryManager : MonoBehaviour
             {
                 int random = Random.Range(0,children.Length);
                 children[i].SetSiblingIndex(random);
+            }
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            Transform[] children = content.GetComponentsInChildren<Transform>();
+            Debug.Log(children.Length);
+            for (int i = 0; i < children.Length; i++)
+            {
+                Debug.Log(children[0].name);
+                Image img = children[i].GetComponent<Image>();
+                print(img);
+                MemoryPiece mp = children[i].GetComponent<MemoryPiece>();
+                print(mp);
+                //children[i].GetComponent<Image>().sprite = children[i].GetComponent<MemoryPiece>().memoryImage;
             }
         }
     }
